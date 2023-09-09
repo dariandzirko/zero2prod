42eb918f6d4a9202e93b89c79fb8f32347adcb2e
Darian <dariandzirko@gmail.com>
2023-09-09 14:10:30 -0600

    I am misreading something because it does not like await'ing after my Result<future>

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+
+    Ok(server)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
+}
